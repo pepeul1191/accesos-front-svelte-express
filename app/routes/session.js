28a662afd2ccd333046d39aca8b70b7e029b2915
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-07-28 09:33:49 -0500

    vistas integradas con svelte, pero aun no hay conexion con las apis

--- app/routes/session.js
+++ app/routes/session.js
@@ -7,4 +7,9 @@ router.get('/sign-in', (req, res) => {
   res.render('sign-in');
 });
 
+router.get('/sign-out', (req, res) => {
+  res.locals.title = 'Inicio';
+  res.render('sign-out');
+});
+
 export default router;
